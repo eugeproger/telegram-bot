eb694d8aa8a91665c45b7990c1ead1353649042c
Euge <euge.itech@gmail.com>
2023-08-30 13:51:35 +0200

    2.4. refactore and use VacansyService

--- src/main/java/com/eugeproger/telegrambot/service/VacancyService.java
+++ src/main/java/com/eugeproger/telegrambot/service/VacancyService.java
@@ -1,10 +1,12 @@
 package com.eugeproger.telegrambot.service;
 
+import com.eugeproger.telegrambot.VacanciesBot;
 import com.eugeproger.telegrambot.dto.VacancyDto;
 import jakarta.annotation.PostConstruct;
-import org.jvnet.hk2.annotations.Service;
+import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -19,11 +21,27 @@ public class VacancyService {
         juniorMaDeveloper.setShortDescription("Java Core is required!");
         vacancies.put("1", juniorMaDeveloper);
 
+        VacancyDto googleDev = new VacancyDto();
+        googleDev.setId("3");
+        googleDev.setTitle("Junior Dev at Google");
+        googleDev.setShortDescription("Welcome to Google!");
+        vacancies.put("3", googleDev);
+
         VacancyDto middle = new VacancyDto();
         middle.setId("1");
         middle.setTitle("Middle Java dev");
         middle.setShortDescription("Join our awesome company!");
         vacancies.put("2", middle);
 
+    }
+
+    public List<VacancyDto> getJuniorVacancies() {
+        return vacancies.values().stream()
+                .filter(v -> v.getTitle().toLowerCase().contains("junior"))
+                .toList();
+    }
+
+    public VacancyDto get(String id) {
+        return vacancies.get(id);
     }
 }
